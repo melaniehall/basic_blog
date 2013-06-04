43329fa063b33d67000e7af86a5a27bd64673488
melaniehall <melaniehalldesign@gmail.com>
2013-06-04 10:33:36 -0500

    User browses list of posts

--- test/integration/anonymous_posts_article_test.rb
+++ test/integration/anonymous_posts_article_test.rb
@@ -5,12 +5,11 @@ class AnonymousPostsArticleTestTest < ActionDispatch::IntegrationTest
       Post.create!(title: "First Post!!", body: "This is awkward... I have nothing to say :(")
       Post.create!(title: "Wow, it's been a while!", body: "And yet, I'm still not a writter :( :( lolol")
 
-      visit '/'
-      click_link "View All Posts"
-      assert_current_path '/posts'
-      # assume we have a page variable
-      assert_include page.content, "First Post!!"
-      #=> assert page.content.include? "First Post!!"
-      assert_include page.content, "Wow, it's been a while!"
+      visit '/posts'
+      sleep 1
+      assert_equal 2, Post.count
+      assert_include page.body, "All Posts"
+      assert_include page.body, "First Post!!"
+      assert_include page.body, "Wow, it's been a while!"
     end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,14 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+require 'capybara/rails'
+Capybara.default_driver = :selenium
+
 class ActiveSupport::TestCase
   # Add helper methods to be used by all tests here...
+end
+
+class ActionDispatch::IntegrationTest
+  include Capybara::DSL
+  self.use_transactional_fixtures = false
 end
