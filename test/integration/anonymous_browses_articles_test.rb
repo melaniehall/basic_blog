32bb15c2dd450a1d1ab8acf61e0a5c227bc5639d
melaniehall <melaniehalldesign@gmail.com>
2013-06-04 10:44:04 -0500

    Fix test name

---
+++ test/integration/anonymous_browses_articles_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class AnonymousBrowsesArticlesTestTest < ActionDispatch::IntegrationTest
+  test "view a list of articles" do
+      Post.create!(title: "First Post!!", body: "This is awkward... I have nothing to say :(")
+      Post.create!(title: "Wow, it's been a while!", body: "And yet, I'm still not a writter :( :( lolol")
+
+      visit '/posts'
+      sleep 1
+      assert_equal 2, Post.count
+      assert_include page.body, "All Posts"
+      assert_include page.body, "First Post!!"
+      assert_include page.body, "Wow, it's been a while!"
+    end
+end
